f3fe89d10f0dc71717c0b687f2deaf293d96288c
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-11-17 12:19:17 +0200

    pr8.1.it

---
+++ UnitTest8.1.re/UnitTest8.1.re.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR08.1/PR08.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81re
+{
+	TEST_CLASS(UnitTest81re)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[] = "asd+=gfd--+";
+			Assert::AreEqual(5, Count(str));
+
+			char* dest = new char[15];
+			dest = Change(str);
+			Assert::AreEqual("asd+=gfd***+", dest);
+		}
+	};
+}
